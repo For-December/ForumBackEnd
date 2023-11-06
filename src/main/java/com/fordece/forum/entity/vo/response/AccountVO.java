760513795e7e4fdb4c5bb3ae69a4ae9ede5f002a
forDecember <1921567337@qq.com>
2023-11-06 17:00:01 +0800

    fixBugs: 统一vo和实体类的类型

--- src/main/java/com/fordece/forum/entity/vo/response/AccountVO.java
+++ src/main/java/com/fordece/forum/entity/vo/response/AccountVO.java
@@ -12,7 +12,7 @@ public class AccountVO implements Serializable {
     String username;
 //    String email; // 邮箱不对前端用户展示，保护隐私捏
     String avatar; // 头像路径（不要存二进制！
-    String balance; // 用户声望什么的
+    Long balance; // 用户声望什么的
     String role;
     Date registerTime;
     Date modifyTime;
