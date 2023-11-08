e77bf73bea72e382204e7510333152d570b2e3a6
forDecember <1921567337@qq.com>
2023-11-08 15:05:31 +0800

    update: 完善参数校验的日志打印

--- src/main/java/com/fordece/forum/controller/exception/CommonController.java
+++ src/main/java/com/fordece/forum/controller/exception/CommonController.java
@@ -4,10 +4,15 @@ package com.fordece.forum.controller.exception;
 import com.fordece.forum.entity.RestBean;
 import jakarta.validation.ValidationException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
@@ -26,7 +31,17 @@ public class CommonController {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<RestBean<Void>> methodArgumentNotValidException(MethodArgumentNotValidException e) {
-        log.warn("Resolve [{}: {}]", "aa", e.getMessage());
+
+        BindingResult bindingResult = e.getBindingResult();
+        if (bindingResult.hasErrors()) {
+            String errorMessage = bindingResult
+                    .getFieldErrors().stream()
+                    .map(t -> t.getDefaultMessage() == null ?
+                            t.getClass().getName() : t.getDefaultMessage())
+                    .collect(Collectors.joining(", "));
+            log.warn("Resolve [{}: {}]", "错误原因", errorMessage);
+
+        }
         return ResponseEntity.badRequest().body(RestBean.failure(400, "请求参数有误，检查一下再试试~（详细日志在后端捏）"));
     }
 
--- src/main/java/com/fordece/forum/entity/vo/request/CreatePostVO.java
+++ src/main/java/com/fordece/forum/entity/vo/request/CreatePostVO.java
@@ -1,16 +1,19 @@
 package com.fordece.forum.entity.vo.request;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CreatePostVO {
 
-    @Size(min = 1, max = 200, message = "帖子内容的长度不合法")
+    @Size(min = 1, max = 200, message = "帖子内容(content)的长度不合法")
     private String content;
 
+    @NotNull(message = "帖子作者不能为空")
     private Long authorId;
 
+    @NotNull(message = "标签必须指定")
     private String tags;
 
 
