5abbca21969403fd8691d69b603cd66007710844
forDecember <1921567337@qq.com>
2023-11-22 14:13:32 +0800

    update: 完善点赞业务

--- src/main/java/com/fordece/forum/service/impl/StarServiceImpl.java
+++ src/main/java/com/fordece/forum/service/impl/StarServiceImpl.java
@@ -1,5 +1,6 @@
 package com.fordece.forum.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.fordece.forum.entity.dto.Star;
 import com.fordece.forum.mapper.StarMapper;
@@ -10,4 +11,43 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class StarServiceImpl extends ServiceImpl<StarMapper, Star> implements StarService {
+    @Override
+    public Page<Star> getStarListByPostId(Long postId) {
+        return null;
+    }
+
+    @Override
+    public void saveStarCountToDB() {
+
+    }
+
+    @Override
+    public void saveStarToRedis() {
+
+    }
+
+    @Override
+    public void unStarFromRedis() {
+
+    }
+
+    @Override
+    public void deleteStarFromRedis() {
+
+    }
+
+    @Override
+    public void incrementStarCount() {
+
+    }
+
+    @Override
+    public void decrementStarCount() {
+
+    }
+
+    @Override
+    public void saveStarsToDB() {
+
+    }
 }
