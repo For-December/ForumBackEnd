df2d7492c27a6b2d9fce87a1cc8a2dfe364e99d4
forDecember <1921567337@qq.com>
2023-12-05 11:08:52 +0800

    update: 错误信息反馈给前端

--- src/main/java/com/fordece/forum/controller/exception/CommonController.java
+++ src/main/java/com/fordece/forum/controller/exception/CommonController.java
@@ -30,8 +30,9 @@ public class CommonController {
     public ResponseEntity<RestBean<Void>> validationException(Exception e) {
         if (e instanceof ValidationException) {
             log.warn("Resolve [{}: {}]", e.getClass().getName(), e.getMessage());
-            return ResponseEntity.badRequest().body(RestBean.failure(400,
-                    "请求参数有误，检查一下再试试~（详细日志在后端捏）"));
+            return ResponseEntity.badRequest().body(RestBean.failure(400, e.getMessage()));
+//            return ResponseEntity.badRequest().body(RestBean.failure(400,
+//                    "请求参数有误，检查一下再试试~（详细日志在后端捏）"));
         }
 
         if (e instanceof MethodArgumentNotValidException) {
@@ -43,10 +44,11 @@ public class CommonController {
                                 t.getClass().getName() : t.getDefaultMessage())
                         .collect(Collectors.joining(", "));
                 log.warn("Resolve [{}: {}]", "错误原因", errorMessage);
+                return ResponseEntity.badRequest().body(RestBean.failure(400, errorMessage));
 
             }
-            return ResponseEntity.badRequest().body(RestBean.failure(400,
-                    "请求参数有误，检查一下再试试~（详细日志在后端捏）"));
+//            return ResponseEntity.badRequest().body(RestBean.failure(400,
+//                    "请求参数有误，检查一下再试试~（详细日志在后端捏）"));
         }
         return ResponseEntity.badRequest().body(RestBean.failure(500, "遇到了意料之外的情况，请联系管理员~"));
 
@@ -55,7 +57,8 @@ public class CommonController {
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<RestBean<Void>> accessDenied(AccessDeniedException e) {
         log.warn("权限不足:{}", e.getMessage());
-        return ResponseEntity.badRequest().body(RestBean.failure(403, "权限不足~（详细日志在后端捏）"));
+        return ResponseEntity.badRequest().body(RestBean.failure(403, e.getMessage()));
+//        return ResponseEntity.badRequest().body(RestBean.failure(403, "权限不足~（详细日志在后端捏）"));
     }
 
 }
